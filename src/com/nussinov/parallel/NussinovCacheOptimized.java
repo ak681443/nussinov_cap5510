55cf17cb24edc0785c5df2036ba55fcdbd59956b
ak681443 <ak681443@gmail.com>
2017-11-30 23:25:48 -0800

    adding cache optimal versions for both sequential & parallel

--- src/com/nussinov/parallel/NussinovCacheOptimized.java
+++ src/com/nussinov/parallel/NussinovCacheOptimized.java
@@ -1,56 +1,53 @@
-package com.nussinov.sequential;
+package com.nussinov.parallel;
 
 import com.nussinov.NussinovSolver;
 import com.nussinov.util.RNA;
 import com.nussinov.util.Util;
 
-public class Nussinov implements NussinovSolver {
+public class NussinovCacheOptimized implements NussinovSolver {
+	
+	public int[][] solve(RNA seq) {
+		char[] sequence = seq.getSequence().toCharArray();
+		int seqLength = seq.getSequence().length();
+		int[][] matrix = new int[seqLength][seqLength];
+		
+		for (int i = 2; i < seqLength; i++) {
+			// omp parallel for
+			for (int j = i; j < seqLength; j++) {
+				int start = j - i;
+				int down = matrix[start + 1][j];
+				int left = matrix[start][j - 1];
+				int diagonal = Util.score(sequence[start], sequence[j]) + matrix[start + 1][j - 1];
+				if (start + 3 <= j) { // Minimum gap
+					int maxValue = 0;
+					for (int k = start + 1; k < j; k++) {
+						int tmp = matrix[start][k] + matrix[j][k+1];
+						if (tmp > maxValue) {
+							maxValue = tmp;
+						}
+					}
+					matrix[start][j] = Math.max(Math.max(down, left), Math.max(diagonal, maxValue));
+				} else {
+					matrix[start][j] = Math.max(Math.max(down, left), diagonal);
+				}
+				matrix[j][start] = matrix[start][j];
+			}
+		}
 
-	@Override
-    public int[][] solve(RNA rnaSequence) {
-		String inpSequence = rnaSequence.getSequence();
-    	int seqLength = inpSequence.length();
-        int[][] dpMatrix = new int[seqLength][seqLength];
-        char sequence[] = inpSequence.toCharArray();
-
-        for (int i = 2; i < seqLength; i++) {
-            for (int j = i; j < seqLength; j++) {
-            	int n = j-i;
-
-            	int case1 = dpMatrix[n+1][j];
-                int case2 = dpMatrix[n][j-1];
-                int case3 = Util.score(sequence[n], sequence[j]) + dpMatrix[n+1][j -1];
-                if(n+3 <= j) {
-                	int temp = 0;
-
-                	for(int k=n+1; k<j; k++) {
-                		if(dpMatrix[n][k] + dpMatrix[k+1][j] > temp) {
-                			temp = dpMatrix[n][k] + dpMatrix[k+1][j];
-                		}
-                	}
-                	int tempMax1 = Math.max(case1, case2);
-        			int tempMax2 = Math.max(case3, temp);
-        			dpMatrix[n][j] = Math.max(tempMax1, tempMax2);
-                } else {
-
-                	int tempMax = Math.max(case1, case2);
-                	dpMatrix[n][j] = Math.max(tempMax, case3);
-                }
-            }
-        }
-        return dpMatrix;
-    }
+		return matrix;
+	}
+	
 	
 	static String ss = "gcgaggcuagcgcuacccgugcgccugcguggaacgauucuguggcgagugccggccgaaagcuagguccggauugcacguggagggccgcccgaagggcacucucggacauuaacccgcauucuguaccauggggcgcaaguuggacccuacgaaggagaagcgggggccaggccgaaaggcccggaagcagaagggugccgagacagaacucgucagauucuugccugcaguaagugacgaaaauuccaagaggcugucuagucgugcucgaaagagggcagccaagaggagauugggcucuguugaagccccuaagacaaauaagucuccugaggccaaaccauugccuggaaagcuaccaaaaggagcuguccagacagcugguaagaagggaccccagucccuauuuaaugcuccucgaggcaagaagcgcccagcaccuggcagugaugaggaagaggaggaggaagacucugaagaagaugguauggugaaccacggggaccucuggggcuccgaggacgaugcugauacgguagaugacuauggagcugacuccaacucugaggaugaggaggaaggugaagcguugcugcccauugaaagagcugcucggaagcagaaggcccgggaagcugcugcugggauccaguggagugaagaggagaccgaggacgaggaggaagagaaagaagugaccccugagucaggccccccaaagguggaagaggcagaugggggccugcagaucaauguggaugaggaaccauuugugcugcccccugcuggggagauggagcaggaugcccaggcuccagaccugcaacgaguucacaagcggauccaggauauugugggaauucugcgugauuuuggggcucagcgggaggaagggcggucucguucugaauaccugaaccggcucaagaaggaucuggccauuuacuacuccuauggagacuuccugcuuggcaagcucauggaccucuucccucugucugagcugguggaguucuuagaagcuaaugaggugccucggcccgucacccuccggaccaauaccuugaaaacccgacgccgagaccuugcacaggcucuaaucaaucgugggguuaaccuggauccccugggcaaguggucaaagacuggacuagugguguaugauucuucugugcccauuggugcuacccccgaguaccuggcugggcacuacaugcugcagggagccuccagcauguugcccgucauggccuuggcaccccaggaacaugagcggauccuggacauguguugugccccuggaggaaagaccagcuacauggcccagcugaugaagaacacgggugugauccuugccaaugacgccaaugcugagcggcucaagaguguugugggcaacuugcaucggcugggagucaccaacaccauuaucagccacuaugaugggcgccaguuccccaaggugguggggggcuuugaccgaguacugcuggaugcucccugcaguggcacuggggucaucuccaaggauccagccgugaagacuaacaaggaugagaaggacauccugcgcugugcucaccuccagaaggaguugcuccugagugcuauugacucugucaaugcgaccuccaagacaggaggcuaccugguuuacugcaccuguucuaucacaguagaagagaaugagugggugguagacuaugcucugaaaaagaggaaugugcgacuggugcccacgggccuagacuuuggccaggaagguuuuacccgcuuucgagaaaggcgcuuccaccccagucugcguucuacccgacgcuucuacccucauacccacaauauggauggguucuucauugccaaguucaagaaauuuuccaauucuaucccucagucccagacaggaaauucugaaacagccacaccuacaaauguagacuugccucaggucauccccaagucugagaacagcagccagccagccaagaaagccaagggggcugcaaagacaaagcagcagcugcagaaacagcaacaucccaagaaggccuccuuccagaagcugaauggcaucuccaaaggggcagacucagaauuguccacuguaccuucugucacaaagacccaagcuuccuccagcuuccaggauagcagucagccagcuggaaaagccgaagggaucagggagccaaaggugacugggaagcuaaagcaacgaucaccuaaauuacaguccuccaagaaaguugcuuuccucaggcagaaugccccucccaagggcacagacacacaaacaccggcuguguuauccccauccaagacucaggccacccugaaaccuaaggaccaucaucagccccuuggaagggccaaggggguugagaagcagcaguugccagagcagccuuuugagaaagcugccuuccagaaacagaaugauacccccaaggggccucagccucccacugugucucccauccguuccagccgccccccaccagcaaagaggaagaaaucucaguccaggggcaacagccagcugcugcuaucuuagaugguugaaaacuagacggguggcucacugccauugucaccagguuggaacucuugccucugugaggaugccuucucuacugugcauacccaugaaauuuaauacacauuuuaaaaccucuggaaaaaaaaaaaaaaaaaaaaaaaa";
 	
 	public static void main(String[] args) {
-		Nussinov o = new Nussinov();
+		NussinovCacheOptimized o = new NussinovCacheOptimized();
 		RNA sequence = RNA.create(ss.toUpperCase(), "");
 		long start = System.currentTimeMillis();
 		int [][] mat = o.solve(sequence);
 		
 		System.out.println(mat[0][mat.length-1]);
 		System.out.println((System.currentTimeMillis()-start)+" millis");
-		System.out.println(Util.doTraceback(sequence, mat));
+		System.out.println(Util.doTraceback(sequence, mat));		
 	}
 }
--- src/com/nussinov/util/Util.java
+++ src/com/nussinov/util/Util.java
@@ -5,6 +5,7 @@ import java.io.Closeable;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Util {
@@ -32,8 +33,55 @@ public class Util {
 	}
 	
 	public static String doTraceback(RNA sequence, int dpMatrix[][]){
-		//TODO: implement traceback later
-		return null;
+		ArrayList<String> list = new ArrayList<>();
+		traceback(0, sequence.getSequence().length()-1, dpMatrix, sequence.getSequence().toCharArray(), list);
+		char result[] = new char[sequence.getSequence().length()];
+		Arrays.fill(result, '.');
+		for(String pair: list){
+			String components[] = pair.split(",");
+			result[Integer.valueOf(components[0])] = '(';
+			result[Integer.valueOf(components[1])] = ')';
+		}
+		return new String(result);
+	}
+	
+	private static void traceback(int i, int j, int N[][],char sequence[], ArrayList<String> result){
+//		if(j <= i)return;
+//		else if(N[i][j] == N[i][j-1]) {
+//			traceback(i, j-1, N,sequence, result);
+//		} else {
+//			for(int k= i; k < j; k++){
+//				if (score(sequence[k], sequence[j])==0) continue;
+//				
+//				if(N[i][j] == N[i][k-1] + N[k+1][j-1] + 1) {
+//					result.add(k+","+j);
+//					traceback(i, k-1, N,sequence, result); 
+//					traceback(k + 1, j-1, N,sequence, result);
+//					return;
+//				}
+//			}
+//		}
+		
+		if(j <= i)return;
+		else if(N[i][j] == N[i+1][j]) {
+			traceback(i+1, j, N,sequence, result);
+		}else if(N[i][j] == N[i][j-1]) {
+			traceback(i, j-1, N,sequence, result);
+		}
+		else if(N[i+1][j-1] + score(sequence[i], sequence[j]) == N[i][j]) {
+			result.add(i+","+j);
+			traceback(i+1, j-1, N,sequence, result);
+		}
+		else {
+			for(int k= i+1; k < j-1; k++){
+				if(N[i][j] == N[i][k] + N[k+1][j]) {
+//					result.add(k+","+j);
+					traceback(i, k, N,sequence, result); 
+					traceback(k + 1, j, N,sequence, result);
+					return;
+				}
+			}
+		}
 	}
 	
 	public static int score(Character base1, Character base2) {
