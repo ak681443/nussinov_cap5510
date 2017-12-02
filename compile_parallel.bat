2212ecee425661b185fdf392d6dc113bc26975b6
ak681443 <ak681443@gmail.com>
2017-12-01 20:57:56 -0800

    windows :/

--- compile_parallel.bat
+++ compile_parallel.bat
@@ -1,1 +1,1 @@
-java -jar lib/omp4j-1.2.jar -d bin -cp bin:. src/com/nussinov/util/*.java src/com/nussinov/*.java src/com/nussinov/**/*.java
+java -jar lib/omp4j-1.2.jar -d bin -cp bin:. src/com/nussinov/util/*.java src/com/nussinov/*.java src/com/nussinov/sequential/*.java src/com/nussinov/parallel/*.java
